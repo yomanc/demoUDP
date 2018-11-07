e597ad4a3b12dc767b7b41a14c39b31ed42569cf
yomanc <yomanc@gmail.com>
2018-11-07 14:59:13 +0800

    1st commit, ok
    sender/receiver

---
+++ ModUdpCli.vb
@@ -0,0 +1,29 @@
+﻿Imports System.Net
+Imports System.Net.Sockets
+Imports System.Text
+
+Module ModUdpCli
+
+    Sub Main()
+        Dim port As Integer = 12345
+        Dim udpClient As New UdpClient()
+        Try
+            udpClient.Connect("localhost", port)
+
+            Console.Write("請輸入你的名字: ")
+            Dim inputString As String = Console.ReadLine()
+            Dim sendBytes As [Byte]() = Encoding.UTF8.GetBytes(inputString)
+            udpClient.Send(sendBytes, sendBytes.Length)
+
+            Dim receiveBytes As [Byte]() = udpClient.Receive(udpClient.Client.LocalEndPoint)
+            Dim returnData As String = Encoding.UTF8.GetString(receiveBytes)
+            Console.WriteLine(returnData.ToString())
+            udpClient.Close()
+        Catch e As Exception
+            Console.WriteLine(e.ToString())
+        End Try
+        Console.ReadKey()
+    End Sub
+
+End Module
+
---
+++ ModUdpSrv.vb
@@ -0,0 +1,31 @@
+﻿Imports System.Net
+Imports System.Net.Sockets
+Imports System.Text
+
+Module ModUdpSrv
+
+    Sub Main()
+        Dim port As Integer = 12345
+        Dim udpClient As New UdpClient(port)
+        Try
+            Dim RemoteIpEndPoint As New IPEndPoint(IPAddress.Any, port)
+
+            While (True)
+                Dim receiveBytes As [Byte]() = udpClient.Receive(RemoteIpEndPoint)
+                Dim receiveData As String = Encoding.UTF8.GetString(receiveBytes)
+                Console.WriteLine("rcv: " & receiveData)
+                Console.WriteLine("frm" & RemoteIpEndPoint.Address.ToString() & ":" & RemoteIpEndPoint.Port)
+
+                Dim sendData As String = "Good day, " & receiveData
+                Dim sendBytes As [Byte]() = Encoding.UTF8.GetBytes(sendData)
+                udpClient.Send(sendBytes, sendBytes.Length, RemoteIpEndPoint)
+                Console.WriteLine("return: " & sendData)
+                Console.WriteLine()
+            End While
+            udpClient.Close()
+        Catch e As Exception
+            Console.WriteLine(e.ToString())
+        End Try
+    End Sub
+
+End Module
